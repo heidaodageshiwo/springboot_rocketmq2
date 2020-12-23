33efcb76bffa4edbc48ddcd73b3e76d9c5d65aa6
zhangqiang <heidaodageshiwo@163.com>
2020-12-23 13:58:47 +0800

    rocketmq

---
+++ rocketmq1/src/main/java/com/rocketmq1/rocketmq1/Rocketmq1Application.java
@@ -0,0 +1,13 @@
+package com.rocketmq1.rocketmq1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rocketmq1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rocketmq1Application.class, args);
+	}
+
+}
---
+++ rocketmq2/src/test/java/com/rocketmq2/rocketmq2/Rocketmq2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketmq2.rocketmq2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rocketmq2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
